f8d9040e310c780114718ff2f8aee3c7114d450b
vishal <vishalpalode9899@gmail.com>
2022-02-10 13:56:15 +0530

    [Vishal] Adding valid first name problem

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -10,8 +10,29 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to user registration program");
-            Console.ReadLine();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("*First letter should be capital.");
+                        Console.WriteLine("*length should be Minimum three characters.");
+                        Console.WriteLine("Enter First name :");
+                        string fname = Console.ReadLine();
+                        ValidRegex Checkpattern = new ValidRegex();
+                        Checkpattern.Checkfirstname(fname);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("invalid option");
+                        break;
+                }
+            }
         }
     }
 }
