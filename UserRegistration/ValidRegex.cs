6acf401d5f1072bf1b34e93f836ece559b2eeecb
vishal <vishalpalode9899@gmail.com>
2022-02-11 10:40:33 +0530

    [VishalPalode] add rule4 special character

--- UserRegistration/ValidRegex.cs
+++ UserRegistration/ValidRegex.cs
@@ -45,10 +45,10 @@ namespace UserRegistration
         }
         public void Checkpassward(string Passward)
         {
-
             //string passwardpattern = "^[A-Za-z0-9]{8,20}$"; 
             // string passwardpattern = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
-            string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+            //string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+            string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
             if (Regex.IsMatch(Passward, passwardpattern))
                 Console.WriteLine("Passward is valid");
             else
